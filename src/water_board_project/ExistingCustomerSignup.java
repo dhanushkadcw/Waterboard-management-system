46a7eea945abdc702e77853366ac3c83886ef294
Dhanushka Weerakoon <dhanushka.dcw@gmail.com>
2024-06-15 20:41:11 +0530

    new customer signup done
    new customer sign up form and new customer show details forms are created and content was added.

--- src/water_board_project/ExistingCustomerSignup.java
+++ src/water_board_project/ExistingCustomerSignup.java
@@ -8,12 +8,12 @@ package water_board_project;
  *
  * @author User
  */
-public class ExistingCustomerLogin extends javax.swing.JFrame {
+public class ExistingCustomerSignup extends javax.swing.JFrame {
 
     /**
      * Creates new form ExistingCustomerLogin
      */
-    public ExistingCustomerLogin() {
+    public ExistingCustomerSignup() {
         initComponents();
     }
 
@@ -121,20 +121,21 @@ public class ExistingCustomerLogin extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(ExistingCustomerLogin.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(ExistingCustomerSignup.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(ExistingCustomerLogin.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(ExistingCustomerSignup.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(ExistingCustomerLogin.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(ExistingCustomerSignup.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(ExistingCustomerLogin.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(ExistingCustomerSignup.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
+        //</editor-fold>
         //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new ExistingCustomerLogin().setVisible(true);
+                new ExistingCustomerSignup().setVisible(true);
             }
         });
     }
