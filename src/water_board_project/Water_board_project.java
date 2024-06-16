73b3de115a4ac10d85b3c0352f79a9716effde50
Dhanushka Weerakoon <dhanushka.dcw@gmail.com>
2024-06-16 20:34:50 +0530

    database connected
    database connected to the project

--- src/water_board_project/Water_board_project.java
+++ src/water_board_project/Water_board_project.java
@@ -3,15 +3,37 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
 package water_board_project;
+import java.sql.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
- * @author User
+ * @author Dhanushka weerakoon
  */
 public class Water_board_project {
 
-    
+    public Water_board_project(){
+        Connect();
+    }
     public static void main(String[] args) {
+        
+    }
+    
+    Connection con;
+    PreparedStatement pst;
+    
+    public void Connect(){
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            con = DriverManager.getConnection("jdbc:mysql://localhost/waterboard_project", "root", "Dhanush#1");
+            System.out.println("Connected");
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(Water_board_project.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (SQLException ex) {
+            Logger.getLogger(Water_board_project.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
         
     }
     
